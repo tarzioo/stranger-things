c9ec3f5627f40dbf01382b575f26f46ee207d662
Taraneh Tayaran <ttayaran@gmail.com>
2016-11-08 22:25:26 +0000

    added a js file

---
+++ static/strangerthings.js
@@ -0,0 +1,6 @@
+"use strict";
+
+$('#message-btn').click(function() {
+    console.log("Entered message function");
+    document.getElementById('six').style.backgroundColor = "rgba(7, 90, 223, 0.5)"; // backcolo
+});
