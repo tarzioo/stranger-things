9dedfa6dd76a6b99b93a50d9dc947bfc9f287abd
Taraneh Tayaran <ttayaran@gmail.com>
2016-11-09 05:02:08 +0000

    added JQuery to make the bulb class add and remove to show it on/off. Currently in testing stage

--- static/strangerthings.js
+++ static/strangerthings.js
@@ -2,5 +2,54 @@
 
 $('#message-btn').click(function() {
     console.log("Entered message function");
-    document.getElementById('six').style.backgroundColor = "rgba(7, 90, 223, 0.5)"; // backcolo
+    document.getElementById('six').style.backgroundColor = "rgba(7, 90, 223, 0.5)";
 });
+
+
+// var on = true;
+// var selector = "#six";
+// var selector = ".bulb";
+// setInterval(function() {
+//     if(on) { 
+//         $(".bulb").addClass('dim');
+//         on = false;
+//     }
+//     else {
+//         $(".bulb").removeClass('dim');
+//         on = true;
+//     }
+// }, 2000);
+
+
+
+// $("#button").click(function() {
+//     var message =  $("#messagebox").val();
+//     // sanitize the input
+//     message = message.trim().toLowerCase().replace(/[^a-z ]+/g, "");
+//     var letter;
+//     var i = 0;
+//     var length = message.length;
+//     var wait = 2000;
+//     var inBetweenWait = 1000;
+
+//     var lightIt = function() {
+//         $("#" + message[i]).addClass('dim');
+//         setTimeout(function() {
+//             $("#" + message[i]).removeClass('dim');
+//             setTimeout(function() {
+//                 i++;
+//                 if(i < length) {
+//                     lightIt();
+//                 }
+//             }, inBetweenWait);
+//         }, wait);
+//     };
+//     lightIt();
+
+//     for (var i=0; i < message.length; i++) {
+//         letter = message[i];
+
+//     }
+
+
+// });
