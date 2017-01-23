2bb048d6abb368d01b3baa04388be6b628d00d06
Taraneh Tayaran <ttayaran@gmail.com>
2017-01-23 04:14:35 +0000

    Created writing functionto be called when event on button happens.  placed lightit function inside writing event

--- static/strangerthings.js
+++ static/strangerthings.js
@@ -7,19 +7,19 @@
 // });
 
 
-var on = true;
-var selector = "#six";
-var selector = ".bulb";
-setInterval(function() {
-    if(on) { 
-        $(".bulb").addClass('dim');
-        on = false;
-    }
-    else {
-        $(".bulb").removeClass('dim');
-        on = true;
-    }
-}, 2000);
+// var on = true;
+// var selector = "#six";
+// var selector = ".bulb";
+// setInterval(function() {
+//     if(on) { 
+//         $(".bulb").addClass('dim');
+//         on = false;
+//     }
+//     else {
+//         $(".bulb").removeClass('dim');
+//         on = true;
+//     }
+// }, 2000);
 
 
 
@@ -88,8 +88,106 @@ $(document).keyup(function(e) {
 /*********************Get message *******************/
 
 
+// $('#send-message-btn').on('click', function (evt) {
+//     var message = $(':input[name=message]').val();
+//     console.log(message);
+//     closeModal();
+//     // sanitize the input
+//     message = message.trim().toLowerCase().replace(/[^a-z ]+/g, "");
+//     console.log(message);
+//     lightIt(message);
+// });
+    
+
+
+// var lightIt = function(message) {
+//     var letter;
+//     var length = message.length;
+//     for (var i=0; i < message.length; i++) {
+//         letter = message[i];
+
+    
+
+//         $("#" + message[i]).removeClass('dim');
+//         console.log(message[i]);
+//         setTimeout(function() {
+//             $("#" + message[i]).addClass('dim');
+//         });
+
+//     }
+        
+
+    
+
+// };
+
+var lightIt = function(message) {
+    var length = message.length;
+    var i = 0;
+    while(i < length) {
+        setInterval(function(){
+            $("#" + message[i]).addClass('dim');
+            }, 2000);
+        
+        i++;
+    }
+};
+
+
+
+
+
+var count = function (num) {
+    if(num < 10){
+    setTimeout(function(){
+        num++;
+        console.log(num);
+        count(num);
+    }, 1000);
+}
+
+}
+
+
+
 $('#send-message-btn').on('click', function (evt) {
     var message = $(':input[name=message]').val();
+    // sanitize the input
+    message = message.trim().toLowerCase().replace(/[^a-z ]+/g, "");
     console.log(message);
-    closeModal();
+   writing(message);
+   closeModal();
+
 });
+
+
+
+var writing = function (message) {
+    console.log("entered writing function");
+    console.log(message);
+    
+    var letter;
+    var i = 0;
+    var length = message.length;
+    var wait = 2000;
+    var inBetweenWait = 1000;
+
+    var lightIt = function() {
+        $("#" + message[i]).addClass('dim');
+        console.log(message[i]);
+        setTimeout(function() {
+            $("#" + message[i]).removeClass('dim');
+            setTimeout(function() {
+                i++;
+                if(i < length) {
+                    lightIt();
+                }
+            }, inBetweenWait);
+        }, wait);
+    };
+    lightIt();
+
+   
+};
+
+
